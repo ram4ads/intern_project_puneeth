c2319be65ef76d4920071c501175d9c9ba28c60f
Puneeth-hub <puneethkumargajjela@gmail.com>
2023-02-06 10:36:08 +0530

    api-client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, gql } from '@apollo/client';
+import "./App.css"
 
-function App() {
+const GET_SUBJECTS = gql`
+  query GetSubjects{
+    messages{
+          items{
+                 subject
+                }       
+              
+              }
+  }
+`
+;
+
+
+
+
+
+export default function App() {
+  const { loading, error, data } = useQuery(GET_SUBJECTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-box'>
+      <h2 className="title-of-header">✔My first Apollo app🤞</h2>
+      <div className="point-to-words">
+      
+        {data.messages.items.map(eachMessage =>(<h3 class="App-link">=>{eachMessage.subject}</h3>))}
+        </div>
     </div>
   );
 }
-
-export default App;
