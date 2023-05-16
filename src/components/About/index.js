902944307181a92ffffde2a05e45500134d641cc
Puneeth-hub <puneethkumargajjela@gmail.com>
2023-05-16 17:55:18 +0530

    navigation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import './index.css'
 
 const About = () =>{
+    
+
     return(
-        <div>
+        <div >
             <h1>About</h1>
             <p>It is a long established fact that a reader will be distracted by the readable content of a page 
                 when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {useState} from 'react'
 import './index.css'
 
 function Header() {
+  const [clicked, setClicked] = useState(0) 
+  const [press, setPress] = useState(0)
+  const [left, setLeft] = useState(0)
+
+  
+  const handleClick = () =>{
+    setClicked(clicked + 1)
+    alert(`Button Clicked ${clicked + 1} times`)
+  }
+  const pressClick = () =>{
+    setPress(press + 1)
+    alert(`Button Clicked ${press + 1} times`)
+  }
+  const leftClick = () =>{
+    setLeft(left + 1)
+    alert(`Button Clicked ${left + 1} times`)
+  }
+
   return (
     <header>
       <nav className='navbar'>
         <ul className='nav-list'>
-          <li><Link to="/">Home</Link></li>|
-          <li><Link to="/about">About</Link></li>|
-          <li><Link to="/contact">Contact</Link></li>|
+          <li onClick = {handleClick}><Link to="/" className='home'>Home</Link></li>|
+          <li onClick= {pressClick}><Link to="/about" className='home'>About</Link></li>|
+          <li onClick = {leftClick}><Link to="/contact" className='home'>Contact</Link></li>|
         </ul>
       </nav>
     </header>
